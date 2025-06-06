bdc90636212912f44a13c9e4082c6a4801aa3d4f
Sumanth <chavalasumanth70@gmail.com>
2025-06-06 21:37:40 +0530

    Update StringCompare.java

--- StringCompare.java
+++ StringCompare.java
@@ -1,7 +1,7 @@
 public class StringCompare {
     public static void main(String[] args) {
-        String str1 = "Sashank";
-        String str2 = "Shashank";
+        String str1 = "sumanth";
+        String str2 = "Sumanth";
 
         if (str1.equals(str2)) {
             System.out.println("Strings are equal (ignoring case)");
