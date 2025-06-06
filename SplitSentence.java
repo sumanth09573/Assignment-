84f283682ae5c980e8d9cb7289f32d3e32f85f64
Sumanth <chavalasumanth70@gmail.com>
2025-06-06 21:36:20 +0530

    Update SplitSentence.java

--- SplitSentence.java
+++ SplitSentence.java
@@ -1,6 +1,6 @@
 public class SplitSentence {
     public static void main(String[] args) {
-        String sentence = "HELLO Bhaii logg mera nam hai Sasank. Arigatho!!";
+        String sentence = "Hi myself sumanth!!";
         String[] words = sentence.split(" ");
 
         for (String word : words) {
