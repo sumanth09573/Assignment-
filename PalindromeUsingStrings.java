46992ea492a25fbb9d4e53f322171a295982bb95
Sumanth <chavalasumanth70@gmail.com>
2025-06-06 21:35:02 +0530

    Update PalindromeUsingStrings.java

--- PalindromeUsingStrings.java
+++ PalindromeUsingStrings.java
@@ -1,6 +1,6 @@
 public class PalindromeUsingStrings {
     public static void main(String[] args) {
-        String str = "SASASAS";
+        String str = "MADAM";
         boolean isPalindrome = true;
         for (int i = 0; i < str.length() / 2; i++) {
             if (str.charAt(i) != str.charAt(str.length() - 1 - i)) {
